72197bd4fa1c0718e1723f6cfcca98a45b486403
ruyou-game <zhanah@ru-you.com>
2016-10-24 16:51:24 +0800

    ss

--- src/main/java/com/zhan/app/common/Video.java
+++ src/main/java/com/zhan/app/common/Video.java
@@ -3,12 +3,14 @@ package com.zhan.app.common;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "brower_app_video")
+@Document(collection = "spider_brower_app_video")
 public class Video {
 	@Id
 	public String id;
 	public String preview_url;
+	public String preview_large_url;
 	public String video_url;
 	public String title;
 	public String detail_html_url;
+	public int browse_count;
 }
